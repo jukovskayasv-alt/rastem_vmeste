57016ed5be91a0da2ff9af106ee308d9f0b0e6fb
jukovskayasv-alt <jukovskayasv@gmail.com>
2026-09-18 23:49:31 +0300

    Use HTTP Yandex server in Android app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,13 @@
 import type { CapacitorConfig } from '@capacitor/cli'
 
-const url=process.env.CAPACITOR_SERVER_URL || 'https://elir.62.84.122.57.nip.io'
+const url=process.env.CAPACITOR_SERVER_URL || 'http://62.84.122.57'
 
 const config: CapacitorConfig = {
   appId: 'ru.elir.personal',
   appName: 'Элир',
   webDir: 'public',
-  server: { url, cleartext: false },
-  android: { allowMixedContent: false }
+  server: { url, cleartext: true },
+  android: { allowMixedContent: true }
 }
 
 export default config
